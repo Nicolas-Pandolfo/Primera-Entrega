50a33d82a3b9318b76057f6970563c20a7407c64
Nicolas Pandolfo <nicolaspandolof@gmail.com>
2022-12-04 20:39:17 -0300

    Agregado do-while y function

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
-let precioProducto = parseInt(prompt("Ingrese el valor del producto:"));
-
-let cuotas = prompt("Ingrese en cuantas cuotas quiere pagar su producto (1,3,6,9,12):");
 let resultado;
+let seguir;
+let continuar = "no";
 
-
-switch(cuotas){
-    case "1":
-        resultado = precioProducto;
-        alert("EL valor de su cuota es de: " + resultado);
-        break;
-    case "3":
-        resultado = precioProducto / 3;
-        alert("El valor de su cuota es de: " + resultado.toFixed(2));
-        break;
-    case "6":
-        resultado = precioProducto / 6;
-        alert("El valor de su cuota es de: " + resultado.toFixed(2));
-        break;
-    case "9":
-        resultado = precioProducto / 9;
-        alert("El valor de su cuota es de: " + resultado.toFixed(2));
-        break;
-    case "12":
-        resultado = precioProducto / 12;
-        alert("El valor de su cuota es de: " + resultado.toFixed(2));
-        break;
-    default:
-        alert("El numero ingresado no es un valor aceptado.");
-        break;
+function division(valorA , valorB){
+    return valorA / valorB;
 }
+do {
+    let precioProducto = parseInt(prompt("Ingrese el valor del producto:"));
+
+    let cuotas = prompt("Ingrese en cuantas cuotas quiere pagar su producto (1,3,6,9,12):");
+
+    switch(cuotas){
+        case "1":
+            resultado = division(precioProducto , 1);
+            console.log("EL valor de su cuota es de: " + resultado);
+            break;
+        case "3":
+            resultado = division(precioProducto , 3);
+            console.log("El valor de su cuota es de: " + resultado.toFixed(2));
+            break;
+        case "6":
+            resultado = division(precioProducto , 6);
+            console.log("El valor de su cuota es de: " + resultado.toFixed(2));
+            break;
+        case "9":
+            resultado = division(precioProducto , 9);
+            console.log("El valor de su cuota es de: " + resultado.toFixed(2));
+            break;
+        case "12":
+            resultado = division(precioProducto , 12);
+            console.log("El valor de su cuota es de: " + resultado.toFixed(2));
+            break;
+        default:
+            alert("El numero ingresado no es un valor aceptado.");
+            break;
+    }
+
+    seguir = prompt("Desea calcular otra cuota?(si/no): ")
+    
+} while (seguir!=continuar);
